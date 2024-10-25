f871f63f9e78d76d3885807bf9f45a9fcb368653
sparty7 <magh080306hgtrzga2@cbtis217.edu.mx>
2024-10-25 13:16:26 -0600

    agregando la base

---
+++ proyecto_transversal.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC1_PC32
+ * Date: 25/10/2024
+ * Time: 01:08 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace proyecto_transversal
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
